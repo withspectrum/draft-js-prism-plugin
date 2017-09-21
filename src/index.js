e7a8dbcd4ec3271fc25c5c54bfe36f36c6247fec
Maximilian Stoiber <contact@mxstbr.com>
2017-09-21 15:59:28 +0200

    Remove rogue console.log

--- src/index.js
+++ src/index.js
@@ -3,11 +3,14 @@ import React from 'react';
 import PrismDecorator from 'draft-js-prism';
 
 type Options = {
-  prism: Object 
+  prism: Object,
 };
 
 const prismPlugin = (options: Options) => {
-  if (!options || !options.prism) throw new Error('You must provide an instance of the prism.js library to draft-js-prism-plugin. See the documentation at https://github.com/withspectrum/draft-js-prism-plugin for more information!')
+  if (!options || !options.prism)
+    throw new Error(
+      'You must provide an instance of the prism.js library to draft-js-prism-plugin. See the documentation at https://github.com/withspectrum/draft-js-prism-plugin for more information!'
+    );
   const { prism } = options;
   return {
     decorators: [
@@ -15,18 +18,19 @@ const prismPlugin = (options: Options) => {
         prism: prism,
         getSyntax(block) {
           const language = block.getData().get('language');
-          console.log(language);
           if (typeof prism.languages[language] === 'object') {
             return language;
           }
           return null;
         },
-        render({ type, children  }) {
-          return <span className={`prism-token token ${type}`}>{children}</span>;
-        }
-      })
-    ]
+        render({ type, children }) {
+          return (
+            <span className={`prism-token token ${type}`}>{children}</span>
+          );
+        },
+      }),
+    ],
   };
-}
+};
 
 export default prismPlugin;
